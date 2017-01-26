48b1236e1b2d6be03ec0e2259d079db274d6fd20
K8406 <K8406@student.jamk.fi>
2017-01-26 14:45:53 +0200

    stuff habend

---
+++ demo03/Car.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace demo03
+{
+    class Car
+    {
+        // properties
+        public string Model { get; set; }
+        public string Color { get; set; }
+        public double Engine { get; set; }
+        public int Speed { get; set; }
+        public bool FuzzyDices { get; set; }
+        public int DoorCount { get; set; }
+        public static int SomeProperty { get; set; }
+
+        // default constructor
+        public Car()
+        {
+
+        }
+
+        // constructor
+        public Car(string model)
+        {
+            Model = model;
+        }
+
+        //method to gib speed
+        public void Accelerate()
+        {
+            Speed += 10;
+        }
+
+        // method to take speed ewey
+
+        public void Brake(int value)
+        {
+            Speed -= value;
+        }
+
+        // method to display car data/properties
+
+        public void PrintData()
+        {
+            Console.WriteLine("Car properties: ");
+            Console.WriteLine("- model: " + Model);
+            Console.WriteLine("- color: " + Color);
+            Console.WriteLine("- engine: " + Engine);
+            Console.WriteLine("- speed: " + Speed);
+            Console.WriteLine("- door count: " + DoorCount);
+            Console.WriteLine("- some shit property: " + SomeProperty);
+            Console.WriteLine("- dices: " + FuzzyDices);
+        }
+
+        // destructor
+        ~Car()
+        {
+            Console.WriteLine("Car is kill. " + "No");
+        }
+
+
+    } // class
+} // namespace
--- demo03/Program.cs
+++ demo03/Program.cs
@@ -10,6 +10,28 @@ namespace demo03
     {
         static void Main(string[] args)
         {
+
+            // create a new instance from Car class
+            Car car = new Car();
+            car.PrintData();
+            car.Model = "Nissan Skyline dr30 RS-X";
+            car.Color = "Red";
+            car.Engine = 2.0;
+            car.Speed = 10;
+            car.FuzzyDices = false;
+            car.DoorCount = 2;
+            car.Accelerate();
+            car.PrintData();
+            car.Brake(7);
+            Car.SomeProperty = 56;
+            car.PrintData();
+
+            // create another car object
+            string model = "Toyota Sprinter Trueno AE86";
+            Car ae86 = new Car(model);
+            ae86.PrintData();
+            ae86.Color = "Panda";
+            Console.WriteLine("Color is " + ae86.Color);
         }
     }
 }
